d5d946e1445eed77fcf4df9d5e89594bb848dfed
Brett Baker <GitBaBB@MyBaBB.com>
2023-12-24 08:53:14 -0800

    rotated image of truck a few  degress

--- src/components/DavesFlag.jsx
+++ src/components/DavesFlag.jsx
@@ -33,7 +33,7 @@ const Instagram = () => {
       <div className='WhiteFlagDiv relative flex justify-center items-center m-auto'
           style={{ height: '33.33%', backgroundColor: 'white' }}>
         <div className='relative block'> 
-                <img src={AndysBlankVan2} alt="DavesBlankVan2" className=' scale-[120%] pl-14
+                <img src={AndysBlankVan2} alt="DavesBlankVan2" className=' scale-[120%] pl-[5rem] rotate-[5deg]
                 mb-[-1rem] xs:mb-[-1.55rem] sm:mb-[-1.55rem] md:mb-[-2.2rem] lg:mb-[-2.2rem] lg:mb-[-2rem] 
                 mt-[-.7rem] xxs:mt-[-.75rem] xs:mt-[-.75rem] sm:mt-[-1.3rem] md:mt-[-1.55rem] lg:mt-[-1.55rem] xl:mt-[-1.7rem]'
                   /> 
