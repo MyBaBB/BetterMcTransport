7d35a9a59b78583c1d2e2c5067ae5d5772868791
Brett Baker <GitBaBB@MyBaBB.com>
2024-02-28 05:19:50 -0800

    footer date

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -6,7 +6,7 @@ function Copyright() {
     <div>
       <p className="fixed bottom-0 right-5 whitespace-nowrap text-xs">
         <span className="text-varBoneWhite dark:text-varLIGHTWING z-0 text-[7px] sm:text-[9px]">
-          &lt;&lt;&lt;&copy;MyBaBB <em>12-2013</em>&gt;&gt;&gt;
+          &lt;&lt;&lt;&copy;MyBaBB <em>12-2023</em>&gt;&gt;&gt;
         </span>
       </p>
     </div>
