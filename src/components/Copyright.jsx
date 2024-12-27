99d182ff55f92a36c520a0ee98ed17e449294969
Brett Baker <GitBaBB@MyBaBB.com>
2024-12-27 09:47:25 -0800

    test cammo bg

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -6,7 +6,7 @@ function Copyright() {
     <div className="bg-transparent relative flex justify-end w-full h-[100px] ">
       <a href="https://mybabb.com">
       <p className="relative w-fit mr-8 h-fit mt-10 p-2 border-[.3px] border-white rounded-lg whitespace-nowrap text-xs bg-black text-white">
-        <span className=" z-0 text-[7px] sm:text-[12px]">
+        <span className=" z-0 text-[9px] sm:text-[12px]">
           &lt;&lt;&lt;&copy;&nbsp;<span className="text-orange-300">Brett&apos;s </span><em>12-2023</em>&gt;&gt;&gt;
         </span>
       </p>
