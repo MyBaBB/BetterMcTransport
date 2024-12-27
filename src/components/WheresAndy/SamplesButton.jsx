c201a667b2e1ac7dcbf3913057aca3188cb85e03
Brett Baker <GitBaBB@MyBaBB.com>
2024-12-27 13:51:13 -0800

    added hot pink

--- src/components/WheresAndy/SamplesButton.jsx
+++ src/components/WheresAndy/SamplesButton.jsx
@@ -6,7 +6,7 @@ const SamplesButton = () => {
       
       <a href="https://custom.email.mybabb.com/">
             <div className="relative font-BlackOpsOne-Regular mb-2 -mt-2   hover:opacity-70 top-4 p-4 px-2 rounded-2xl
-             bg-blue-950 text-white border-[.5px] border-white">
+             bg-blue-950 text-[hotpink] border-[.5px] border-white">
                To Samples
             </div>
 </a>  
